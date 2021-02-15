262a7a4906c1380fcb83d59c4ba1d4e921c6413c
Manikandan Sundararajan <mani@hey.com>
2021-02-15 12:48:21 -0500

    Move db related functions to db file

---
+++ src/athens_transit_md/db.clj
@@ -0,0 +1,41 @@
+(ns athens-transit-md.db
+  (:require [datascript.core :as d])
+  (:gen-class))
+
+
+(def INDEX_FILE "index.transit") ;; Default path to the index.transit file
+(def dsdb (atom #{}))            ;; dsdb is initially an empty atom map
+
+
+(defn e-by-av
+  [a v]
+  (-> (d/datoms @dsdb :avet a v) first :e))
+
+
+(defn get-all-pages
+  "Gets the datoms for all the pages in the DB"
+  []
+  (let [query '[:find ?t
+                :where [?t :node/title]]]
+    (->> @dsdb
+         (d/q query)
+         (reduce (fn [acc v] (conj acc (first v))) '[])
+         (d/pull-many @dsdb '[*]))))
+
+
+(defn sort-block-children
+  [block]
+  (if-let [children (seq (:block/children block))]
+    (assoc block :block/children
+           (vec (sort-by :block/order (map sort-block-children children))))
+    block))
+
+
+(def block-document-pull-vector
+  '[:db/id :block/uid :block/string :block/open :block/order {:block/children ...} :block/refs :block/_refs])
+
+
+(defn get-block-document
+  [id]
+  (->> (d/pull @dsdb block-document-pull-vector id)
+       (sort-block-children)))
