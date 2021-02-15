7d1bb98fbd35fa5f4ed25f2b2b3220e1a0ae758a
Manikandan Sundararajan <mani@hey.com>
2021-02-15 12:49:57 -0500

    Update function call namespaces

--- src/athens_transit_md/parse.clj
+++ src/athens_transit_md/parse.clj
@@ -1,33 +1,21 @@
 (ns athens-transit-md.parse
-  (:require [datascript.transit :as dt]
-            [datascript.core :as d])
+  (:require [athens-transit-md.db :as db]
+            [datascript.transit :as dt])
   (:gen-class))
 
-(def INDEX_FILE "index.transit") ;; Default path to the index.transit file
-(def dsdb (atom #{}))            ;; dsdb is initially an empty atom map
 
 (defn read-transit-file
   "Reads the index.transit file from the path given and loads it into the dsdb atom"
   ([]
-   (read-transit-file INDEX_FILE))
+   (read-transit-file db/INDEX_FILE))
   ([index-file-path]
    (let [transit-file (slurp index-file-path)
          dt-db (dt/read-transit-str transit-file)]
-     (reset! dsdb dt-db))))
+     (reset! db/dsdb dt-db))))
 
-(defn e-by-av
-  [a v]
-  (-> (d/datoms @dsdb :avet a v) first :e))
 
-(defn get-all-pages
-  "Gets the datoms for all the pages in the DB"
-  []
-  (let [query '[:find ?t
-                :where [?t :node/title]]]
-    (->> @dsdb
-         (d/q query)
-         (reduce (fn [acc v] (conj acc (first v))) '[])
-         (d/pull-many @dsdb '[*]))))
+(defn now [] (new java.util.Date))
+
 
 (defn walk-str
   "Four spaces per depth level."
@@ -37,27 +25,11 @@
         walk-children (apply str (map #(walk-str (inc depth) %) children))]
     (str left-offset "- " string "\n" walk-children)))
 
-(defn sort-block-children
-  [block]
-  (if-let [children (seq (:block/children block))]
-    (assoc block :block/children
-           (vec (sort-by :block/order (map sort-block-children children))))
-    block))
-
-
-(def block-document-pull-vector
-  '[:db/id :block/uid :block/string :block/open :block/order {:block/children ...} :block/refs :block/_refs])
-
-
-(defn get-block-document
-  [id]
-  (->> (d/pull @dsdb block-document-pull-vector id)
-       (sort-block-children)))
 
 (defn export-to-md
   [uid]
-  (let [eid (e-by-av :block/uid uid)
-        block (get-block-document eid)
+  (let [eid (db/e-by-av :block/uid uid)
+        block (db/get-block-document eid)
         title (:node/title block)
         block-children (:block/children block)]
     (prn (str title " - Exporting page to markdown"))
@@ -65,9 +37,10 @@
          (map #(walk-str 0 %))
          (apply str))))
 
+
 (defn export-all-pages
   []
-  (let [all-pages (get-all-pages)
+  (let [all-pages (db/get-all-pages)
         all-page-uids (map :block/uid all-pages)
         all-md (map export-to-md all-page-uids)]
     (prn all-md)))
